155ff89e4a3062bdc65ef80f1dd6ba164bf4ce73
SeongJoon95 <gnsxmtkfkd@naver.com>
2024-08-21 17:43:47 +0900

    2024-08-21 17:43

--- src/main/java/com/seongjoon/springbasic/controller/RequestDateController.java
+++ src/main/java/com/seongjoon/springbasic/controller/RequestDateController.java
@@ -1,6 +1,7 @@
 package com.seongjoon.springbasic.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,12 +21,48 @@ public class RequestDateController {
   // http://localhost:4000/request-data/request-param?name=홍길동&age=30
   public String requestParam(
       // @RequestParam(name="name") String name,
-    @RequestParam() String name1,
+    @RequestParam("name") String name,
     @RequestParam(name="age", required=false) Integer age
     
   ) {
-    
-    return "이름 : "+ name1 +" 나이 : "+age;
+
+    return "이름 : " + name + " 나이 : " + age;
   }
 
+  // @PathVariable():
+  // 모든 HTTP 메서드에서 URL의 특정 패턴에 따라 데이터를 추출
+  // GET http://localhost:4000/request-data/path-variable/*/*  */ == ' * '  : 변수로 받고싶은 값이라고 했을 경우
+  @GetMapping(
+    {"/path-variable/{var}/{str}",
+      "/path-variable/{var}/",
+      "/path-variable/{var}"
+  })
+  public String pathVariable(
+    @PathVariable(name="var") String var,
+      @PathVariable(name="str", required = false) String str
+  ) {
+    return "읽은경로 변수 :" + var + " 두번째 매개변수 : " + str;
+  }
+  
+  // ! 주의
+  // 경로 변수를 사용하여 URL 패턴을 작성할 때
+  // 겹치는 패턴이 존재하는지 잘 확인해야 함
+  @GetMapping("/path-variable/other")
+  public String otherPathVariable() {
+    return "other 메서드 호출";
+  }
+
+  @GetMapping("/path-variable/{var}/another")
+  public String anotherPathVariable1(
+    @PathVariable("var") String var
+  ) {
+    return "another1";
+  }
+
+  @GetMapping("/path-variable/another/{var}")
+  public String anotherPathVariable2(
+    @PathVariable("var") String var
+  ) {
+    return "another2";
+  }
 }
